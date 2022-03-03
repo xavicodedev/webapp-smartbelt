34a87a82e03fd0ec890d36bc4e5673ab203ffae5
Xavi Garcia <crafttutos@gmail.com>
2022-03-03 09:27:32 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+var XMLHttpRequest = require('xhr2');
+var xhr = new XMLHttpRequest();
+
+
+
+var request = new XMLHttpRequest();  
+request.open('GET', 'https://service-smartbelt.herokuapp.com/', true);
+request.onreadystatechange = function(){
+    if (request.readyState === 4){
+        if (request.status === 404) {  
+            alert("EL servidor service-smartbelt está APAGADO...");
+        }  else {
+            app.get('/', function (req, res) {
+                res.send('EL servidor service-smartbelt está FUNCIONANDO...');
+             })
+        }
+    }
+};
+
+request.send();
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
